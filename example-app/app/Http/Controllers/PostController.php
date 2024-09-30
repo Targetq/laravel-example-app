a026b6c544a5e8a69912e14a84588ea4dc2f80b7
Deniss <your@email.example>
2024-09-30 15:40:13 +0300

    dd, log, user can see all posts

--- example-app/app/Http/Controllers/PostController.php
+++ example-app/app/Http/Controllers/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::all();
+        //\Log::debug($posts);
+        //dd($posts);
+        return view('posts.index', ['allposts'=>$posts]);
+    }
 }
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\PostController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts', [PostController::class, 'index']);
